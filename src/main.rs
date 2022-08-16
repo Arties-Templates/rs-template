5aa01303fabcae8903d38a45e5851fc939a95c72
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-08-16 15:40:28 +1000

    Add `anyhow`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
+use anyhow::Result;
 
-fn main() {
+fn main() -> Result<()> {
     let args = env::args().skip(1);
 
     for arg in args {
       println!("{}", arg)
     }
+
+    Ok(())
 }
